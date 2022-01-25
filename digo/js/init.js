f2d1d3d69d9e8a7e469370fd7c3d55c78b6f3f9e
ContiRodrigo <98416350+ContiRodrigo@users.noreply.github.com>
2022-01-25 15:22:56 -0300

    Add files via upload

---
+++ digo/js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDDFSsgb2EnsvbWwOW0wWlrqoFmm9yf0bQ",
+    authDomain: "todo-list-9d178.firebaseapp.com",
+    projectId: "todo-list-9d178",
+    storageBucket: "todo-list-9d178.appspot.com",
+    messagingSenderId: "36709069303",
+    appId: "1:36709069303:web:8865785727aa638b15fef2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
